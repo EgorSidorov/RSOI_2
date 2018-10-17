f8d5ba35300d14b53d5297f667c9424a40d81730
sidorov.ea@skpari.local <sidorov.ea@skpari.local>
2018-10-17 14:48:37 +0300

    1

--- src/test/java/AccountModelTest.java
+++ src/test/java/AccountModelTest.java
@@ -25,34 +25,34 @@ public class AccountModelTest {
     @Test
     public void getDbStatus() throws Exception {
         AccountModel model = new AccountModel();
-        assertEquals(true,model.GetDbStatus());
+        Assert.assertEquals(true,model.GetDbStatus());
     }
 
     @Test
     public void createConnection() throws Exception {
         AccountModel model = new AccountModel();
-        assertEquals(true,model.CreateConnection());
+        Assert.assertEquals(true,model.CreateConnection());
     }
 
     @Test
     public void getAllRoles() throws Exception {
         AccountModel model = new AccountModel();
         model.GetAllRoles();
-        assertEquals(true,model.queryStatus);
+        Assert.assertEquals(true,model.queryStatus);
     }
 
     @Test
     public void CreateUser() throws Exception {
         AccountModel model = new AccountModel();
-        assertEquals(false,model.CreateUser("Egor","qwerty","1"));
+        Assert.assertEquals(false,model.CreateUser("Egor","qwerty","1"));
     }
 
     @Test
     public void Login() throws Exception {
         AccountModel model = new AccountModel();
         String token = model.Login("Egor2","qwerty");
-        assertEquals(true,model.queryStatus);
-        assertEquals(false,token.isEmpty());
+        Assert.assertEquals(true,model.queryStatus);
+        Assert.assertEquals(false,token.isEmpty());
     }
 
     @Test
@@ -60,30 +60,30 @@ public class AccountModelTest {
         AccountModel model = new AccountModel();
         String token = model.Login("Egor3","qwerty");
         List<String> listString = model.GetUserNames(token,0);
-        assertEquals(true,model.GetQueryStatus());
-        assertEquals("Egor3",model.GetUsername(token));
-        assertEquals(true,model.GetQueryStatus());
+        Assert.assertEquals(true,model.GetQueryStatus());
+        Assert.assertEquals("Egor3",model.GetUsername(token));
+        Assert.assertEquals(true,model.GetQueryStatus());
     }
 
     @Test
     public void Logout() throws Exception {
         AccountModel model = new AccountModel();
         String token = model.Login("Egor4","qwerty");
-        assertEquals(true,model.Logout(token));
+        Assert.assertEquals(true,model.Logout(token));
     }
 
     @Test
     public void GetUsername() throws Exception {
         AccountModel model = new AccountModel();
         String token = model.Login("Egor5","qwerty");
-        assertEquals("Egor5",model.GetUsername(token));
+        Assert.assertEquals("Egor5",model.GetUsername(token));
     }
 
     @Test
     public void GetRole() throws Exception {
         AccountModel model = new AccountModel();
         String token = model.Login("Egor6","qwerty");
-        assertEquals("master",model.GetRole(token));
+        Assert.assertEquals("master",model.GetRole(token));
     }
 
 }

81020bf3cf7463205e4616d82c90510c632e4962
EgorSidorov <sega-96@mail.ru>
2018-10-17 17:29:29 +0300

    Update Startup.java

--- src/main/java/Startup.java
+++ src/main/java/Startup.java
@@ -8,7 +8,7 @@ public class Startup
         //private static String MSSQLDRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
         
         public static String GetConnectionStr() {
-            return "jdbc:mysql://127.0.0.1:3306/test";
+            return "jdbc:mysql://127.0.0.1:3306/test?user=root&password=&useUnicode=true";
         }
 
         public static String GetDriver() {

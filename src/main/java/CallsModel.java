f61c7911ad58e8c81a8ed80fec4293a87bf82e7b
EgorSidorov <sega-96@mail.ru>
2018-10-17 17:30:03 +0300

    Update CallsModel.java

--- src/main/java/CallsModel.java
+++ src/main/java/CallsModel.java
@@ -28,7 +28,7 @@ public class CallsModel {
             return false;
         }
         try {
-            connection = DriverManager.getConnection(db_uri,"root","");
+            connection = DriverManager.getConnection(db_uri);
 
         } catch (SQLException e) {
             System.out.print("\nError get connection "+e.getMessage() + "\n");

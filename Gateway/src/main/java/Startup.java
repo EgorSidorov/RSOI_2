3b032a646ea132e525923bd95218f0d67090b489
EgorSidorov <sega-96@mail.ru>
2018-10-24 01:18:41 +0300

    Update Startup.java

--- Gateway/src/main/java/Startup.java
+++ Gateway/src/main/java/Startup.java
@@ -1,7 +1,11 @@
 public class Startup
 {
         private static String gatewayHostPort = "194.58.121.174:8080";
+        private static String restApiCommand = "http://194.58.121.174:8080/?service=Cross&arguments=\"ADD_CALL\"&Duration=20";
         public static String GetGatewayHostPort() {
             return gatewayHostPort;
+        }
+        public static String getRestApiCommand() {
+            return restApiCommand;
         }
 }
